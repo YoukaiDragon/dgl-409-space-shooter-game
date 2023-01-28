d95e546e0b624329e5e2240296caab972993d222
YoukaiDragon <tyson.foster@hotmail.com>
2023-01-27 19:29:52 -0800

    Add viewport object

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById("gameWindow");
 const ctx = canvas.getContext("2d");
+
+const viewport = {
+    width: canvas.width,
+    height: canvas.height,
+    x: 0,
+    y: 0
+}
+
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
