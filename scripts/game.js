63e18bdcc833f6f929f0860840fd9332afe8e136
YoukaiDragon <tyson.foster@hotmail.com>
2023-01-27 19:34:17 -0800

    Create Player class constructor

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,16 @@
+class Player {
+    constructor(x, y) {
+        this.lives = 3;
+        this.score = 0;
+        this.x = x;
+        this.y = y;
+        this.speed = 0;
+        this.angle = 0;
+        this.bombs = 3;
+        this.shotType = 0;
+    }
+}
+
 const canvas = document.getElementById("gameWindow");
 const ctx = canvas.getContext("2d");
 
