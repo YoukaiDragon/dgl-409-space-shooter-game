7a204e9ae78c392e92fcf8afcced64a40caa61d4
YoukaiDragon <tyson.foster@hotmail.com>
2023-01-30 23:03:57 -0800

    Add Bullet object

---
+++ scripts/gameObjects/bullet.js
@@ -0,0 +1,18 @@
+class Bullet {
+    constructor(x, y, angle, speed, duration, playerOwned) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.speed = speed;
+        this.duration = duration;
+        this.playerOwned = playerOwned;
+    }
+
+    update() {
+
+    }
+
+    render(viewport, canvas, ctx, displayX, displayY) {
+
+    }
+}
