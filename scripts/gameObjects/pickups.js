9891148de0c203517b1d67d18a9859e28e53baed
YoukaiDragon <tyson.foster@hotmail.com>
2023-02-01 13:11:28 -0800

    Add pickup parent class

---
+++ scripts/gameObjects/pickups.js
@@ -0,0 +1,13 @@
+class Pickup {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width;
+        this.height;
+        this.duration
+    }
+
+    update() { }
+    render(viewport, canvas, ctx, displayX, displayY) { }
+    onPickup() { }
+}
