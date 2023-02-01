faaa641e8986e69930d21054f4deb64bd3514fcd
YoukaiDragon <tyson.foster@hotmail.com>
2023-02-01 13:35:47 -0800

    Add render code and temp appearance for timePickup

--- scripts/gameObjects/pickups.js
+++ scripts/gameObjects/pickups.js
@@ -10,21 +10,31 @@ class Pickup {
     update() {
         this.duration--;
     }
-    render(viewport, canvas, ctx, displayX, displayY) { }
+    render(viewport, canvas, ctx) { }
     onPickup() { }
 }
 
 class timePickup extends Pickup {
     constructor(x, y) {
         super(x, y);
+        this.width = 20;
+        this.height = 20;
     }
 
     update() {
         super.update();
     }
 
-    render(viewport, canvas, ctx, displayX, displayY) {
-
+    render(viewport, canvas, ctx) {
+        let displayX = this.x - viewport.x;
+        let displayY = this.y - viewport.y;
+        if (displayX < this.width * -1 || displayX > viewport.width
+            || displayY < this.height * -1 || displayY > viewport.height) {
+                return; // Out of viewport, do not display
+        }
+        ctx.beginPath();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(displayX, displayY, this.width, this.height);
     }
 
     onPickup() {
