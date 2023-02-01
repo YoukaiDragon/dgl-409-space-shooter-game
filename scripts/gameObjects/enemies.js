543fb9ea260f0504201b0c676a71e638b25bd6b9
YoukaiDragon <tyson.foster@hotmail.com>
2023-01-31 19:52:26 -0800

    Add code to update enemy and bullet positions

--- scripts/gameObjects/enemies.js
+++ scripts/gameObjects/enemies.js
@@ -15,7 +15,18 @@ class Enemy {
     }
 
     update() {
-
+        // Move player based on speed and angle
+        // Formula based on https://stackoverflow.com/questions/36955714/calculating-cordanates-with-angles
+        this.x += Math.cos(this.angle * (Math.PI / 180)) * this.speed;
+        this.y += Math.sin(this.angle * (Math.PI / 180)) * this.speed;
+        if (this.bullets.length > 0) {
+            for (let i = this.bullets.length - 1; i >= 0; i--) {
+                this.bullets[i].update();
+                if (this.bullets[i].duration <= 0) {
+                    this.bullets.splice(i, 1);
+                }
+            }
+        }
     }
     render() {
 
