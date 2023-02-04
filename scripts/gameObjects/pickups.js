3c5bfa476b8caf08b844766874ad1c0caccc653a
YoukaiDragon <tyson.foster@hotmail.com>
2023-02-03 23:36:24 -0800

    Create point pickups

--- scripts/gameObjects/pickups.js
+++ scripts/gameObjects/pickups.js
@@ -41,3 +41,48 @@ class timePickup extends Pickup {
         timer += 10;
     }
 }
+
+class scorePickup extends Pickup {
+    constructor(x, y, size = 'sm') {
+        super(x, y);
+        switch(size) {
+            case 'lg':
+                this.width = 40;
+                this.height= 40;
+                this.points = 20;
+                break;
+            case 'md':
+                this.width = 20;
+                this.height = 20;
+                this.points = 10;
+                break;
+            case 'sm':
+            default:
+                this.width = 10;
+                this.height = 10;
+                this.points = 5;
+                break;    
+        }
+        this.duration = 2000;
+    }
+
+    update() {
+        super.update();
+    }
+
+    render(viewport, canvas, ctx) {
+        let displayX = this.x - viewport.x;
+        let displayY = this.y - viewport.y;
+        if (displayX < this.width * -1 || displayX > viewport.width
+            || displayY < this.height * -1 || displayY > viewport.height) {
+                return; // Out of viewport, do not display
+        }
+        ctx.beginPath();
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(displayX, displayY, this.width, this.height);
+    }
+
+    onPickup() {
+        score += this.points;
+    }
+}
