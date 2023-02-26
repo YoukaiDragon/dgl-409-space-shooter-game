a1acf971f9228140acf94d113fade420ac6b1bbd
YoukaiDragon <tyson.foster@hotmail.com>
2023-02-25 22:15:03 -0800

    Change what class AdvancedShooterEnemy extends

--- scripts/gameObjects/enemies.js
+++ scripts/gameObjects/enemies.js
@@ -153,15 +153,13 @@ class ShooterEnemy extends Enemy {
         explosion1.currentTime = 0;
         explosion1.play();
         let dropValue = Math.random() * 10;
-        if (this.points == 3) { dropValue += 3 } // Have Advanced Shooter Enemies drop better items
-        if (dropValue > 9) { return 'scoreLG' }
         if (dropValue > 7) { return 'scoreMD' }
         if (dropValue > 3) { return 'scoreSM' }
         return '';
     }
 }
 
-class AdvancedShooterEnemy extends ShooterEnemy {
+class AdvancedShooterEnemy extends Enemy {
     constructor(x, y) {
         super(x, y);
         this.hp = 5;
@@ -171,6 +169,70 @@ class AdvancedShooterEnemy extends ShooterEnemy {
         this.width = 60;
         this.height = this.width;
         this.colour = "purple";
+    }
+
+    update() {
+        let distance = this.getPlayerDistance();
+        if (distance <= this.aggroDistance) {
+            // Accelerate to max speed
+            if (distance <= 400 && this.speed > this.maxSpeed / 4) {
+                // Keep some distance from the player
+                this.speed--;
+            } else if (this.speed < this.maxSpeed) { this.speed++; }
+
+            // Turn to face player
+            let angleToPlayer = this.getPlayerAngle();
+            let angleDiff = angleToPlayer - this.angle;
+            angleDiff < 0 ? angleDiff += 360 : angleDiff;
+            if (angleDiff < this.turnSpeed || angleDiff > 360 - this.turnSpeed) {
+                // When angle difference is less than turn speed, snap to face player
+                this.angle = angleToPlayer;
+            } else {
+                // Pick shortest turn distance to player
+                angleDiff < 180 ? this.angle += this.turnSpeed : this.angle -= this.turnSpeed;
+            }
+
+            // Fire bullets
+            if (this.nextShotTime > 0) {
+                this.nextShotTime--;
+            } else {
+                if (angleDiff < 4 * this.turnSpeed || angleDiff > 360 - 4 * this.turnSpeed) {
+                    this.bullets.push(new Bullet(this.x, this.y, this.angle, 20, 100, 6, false));
+                    this.nextShotTime = this.fireRate;
+                }
+            }
+        } else if (this.speed > 0) {
+            this.speed--;
+        }
+        super.update();
+    }
+
+    render(viewport, canvas, ctx) {
+        let displayX = this.x - viewport.x;
+        let displayY = this.y - viewport.y;
+        if (isVisible(displayX, displayY)) {
+            ctx.beginPath();
+            ctx.fillStyle = this.colour;
+            ctx.translate(displayX, displayY);
+            ctx.rotate(this.angle * Math.PI / 180);
+            ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+            ctx.rotate(-(this.angle * Math.PI / 180));
+            ctx.translate(-displayX, -displayY);
+        }
+
+        super.render(viewport, canvas, ctx);
+    }
+
+    onDeath() {
+        super.onDeath();
+        explosion1.currentTime = 0;
+        explosion1.play();
+        let dropValue = Math.random() * 10;
+        if (this.points == 3) { dropValue += 3 } // Have Advanced Shooter Enemies drop better items
+        if (dropValue > 6) { return 'scoreLG' }
+        if (dropValue > 4) { return 'scoreMD' }
+        if (dropValue > 0) { return 'scoreSM' }
+        return '';
     }
 }
 
